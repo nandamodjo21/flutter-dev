39a921ea985a3f9508a6671136ebb72b960ec52a
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-11-02 17:02:52 +0800

    fix: update widget login

--- lib/app/data/module/view/widget/login_widget.dart
+++ lib/app/data/module/view/widget/login_widget.dart
@@ -29,3 +29,51 @@ AppBar buildAppBar(){
     ),
   );
 }
+
+Widget buildLogin(BuildContext context){
+  return Center(
+    child: Container(
+      padding: EdgeInsets.only(
+        left: 50.w,
+        right: 50.w
+      ),
+      margin: EdgeInsets.only(
+        top: 40.h,
+        bottom: 20.h
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _reusableIcons("facebook"),
+          _reusableIcons("apple"),
+          _reusableIcons("google"),
+        ],
+      )
+    ),
+  );
+}
+Widget _reusableIcons(String iconName){
+  return GestureDetector(
+    onTap: (){
+
+    },
+    child: SizedBox(
+      width: 40.w,
+      height: 40.w,
+      child: Image.asset("assets/icons/$iconName.png"),
+    ),
+  );
+}
+Widget reusableText(String text){
+ return Container(
+    margin:EdgeInsets.only(bottom: 5.h,) ,
+   child: Text(
+     text,
+     style: TextStyle(
+       fontSize: 15.sp,
+       color: Colors.grey.withOpacity(0.5),
+       fontWeight: FontWeight.normal
+     ),
+   ),
+  );
+}
