a5fec52154bdd5148cb79e1686746e67d31673aa
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-11-03 16:38:23 +0800

    fix: update  login widget

--- lib/app/data/module/view/widget/login_widget.dart
+++ lib/app/data/module/view/widget/login_widget.dart
@@ -1,5 +1,7 @@
+import 'package:absensi/common/values/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 AppBar buildAppBar(){
   String login ="LOGIN";
@@ -9,7 +11,7 @@ AppBar buildAppBar(){
     bottom: PreferredSize(
       preferredSize: const  Size.fromHeight(1.0),
       child: Container(
-        color: Colors.grey.withOpacity(0.5),
+        color: AppColors.primarySecondaryBackground,
         height: 1.0,
 
       ),
@@ -18,7 +20,7 @@ AppBar buildAppBar(){
       child: Text(
         login,
         style: TextStyle(
-            color: Colors.black,
+            color: AppColors.primaryText,
             fontFamily: "Roboto",
             fontStyle: FontStyle.normal,
             fontSize: 20.sp,
@@ -75,5 +77,158 @@ Widget reusableText(String text){
        fontWeight: FontWeight.normal
      ),
    ),
+  );
+}
+Widget reusableField(String text){
+  return Container(
+    margin:EdgeInsets.only(bottom: 5.h),
+    child: Text(
+      text,
+      style: TextStyle(
+          fontSize: 15.sp,
+          color: Colors.black,
+          fontWeight: FontWeight.normal
+      ),
+    ),
+  );
+}
+Widget buildTextField(String text, String textType, String iconName, void Function(String value)? func
+    ){
+  return Container(
+    width: 325.w,
+    height: 50.h,
+    margin: EdgeInsets.only(bottom: 20.h),
+    decoration: BoxDecoration(
+      color: Colors.grey.withOpacity(0.1),
+      borderRadius: BorderRadius.all(Radius.circular(15.w)),
+        border: Border.all(color: AppColors.primaryFourElementText)
+    ),
+    child: Row(
+      children: [
+        Container(
+          width: 16.w,
+          margin: EdgeInsets.only(left: 17.w),
+          height: 16.w,
+          child: Image.asset("assets/icons/$iconName.png"),
+        ),
+        SizedBox(
+          width: 270.w,
+          height: 50.h,
+          child: TextField(
+            onChanged:(value)=>func!(value) ,
+            keyboardType: TextInputType.multiline,
+            decoration: InputDecoration(
+              hintText: text,
+              border: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.transparent
+                )
+              ),
+              enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.transparent
+                  )
+              ),
+              disabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.transparent
+                  )
+              ),
+              focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                      color: Colors.transparent
+                  )
+              ),
+              hintStyle: TextStyle(
+                color: AppColors.primarySecondaryElementText,
+                height: 2.w
+              ),
+
+            ),
+            style: TextStyle(
+              color: AppColors.primaryText,
+              fontFamily: "Avenir",
+              fontWeight: FontWeight.normal,
+              fontSize: 14.sp
+            ),
+            autocorrect: false,
+            obscureText: textType=="password"?true:false,
+          ),
+        )
+      ],
+    ),
+  );
+}
+Widget forgotPass( ){
+  return Container(
+    margin: EdgeInsets.only(left: 25.w),
+    width: 260.w,
+    height: 44.h,
+    child: GestureDetector(
+      onTap: (){
+
+      },
+      child: Text(
+        "lupa password?",
+        style: TextStyle(
+          color: AppColors.primaryText,
+          decoration: TextDecoration.underline,
+          fontSize: 12.sp,
+          decorationColor: AppColors.primaryText
+        ),
+      ),
+    ),
+  );
+}
+Widget buildButtonLogAndReg(String buttonName, String buttonType, void Function()? func){
+  return GestureDetector(
+    onTap: ()=> func,
+//       {
+// showToast();
+//     },
+    child: Container(
+      width: 325.w,
+      height: 50.h,
+      margin: EdgeInsets.only(left: 25.w, right: 25.w, top: buttonType=="login"?40.h:20.h),
+      decoration: BoxDecoration(
+        color:buttonType =="login"?AppColors.primaryElement:AppColors.primaryBackground,
+        borderRadius: BorderRadius.circular(15.w),
+        border: Border.all(
+          color: buttonType =="login"?Colors.transparent:AppColors.primaryFourElementText
+        ),
+        boxShadow: [
+          BoxShadow(
+            spreadRadius: 1,
+            blurRadius: 2,
+            offset: const Offset(0, 1),
+            color: Colors.grey.withOpacity(0.1)
+          )
+        ]
+      ),
+      child: Center(
+        child: Text(
+          buttonName,
+          style: TextStyle(
+            color:buttonType =="login"? AppColors.primaryBackground:AppColors.primaryText,
+            fontSize: 16.sp,
+            fontWeight: FontWeight.normal,
+
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+
+void showToast() {
+  Fluttertoast.showToast(
+    msg: "berhasil",
+    toastLength: Toast.LENGTH_SHORT, // Durasi tampilan toast
+    gravity: ToastGravity.BOTTOM, // Posisi toast (TOP, CENTER, BOTTOM)
+    timeInSecForIosWeb: 1, // Durasi tampilan untuk platform iOS
+    backgroundColor: Colors.black87, // Warna latar belakang toast
+    textColor: Colors.white, // Warna teks pada toast
+    fontSize: 16.0, // Ukuran teks
   );
 }
