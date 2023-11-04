4b31fa37b50a2ba1b4452f9840a793c8f9b1c384
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-11-04 17:00:35 +0800

    fix: update login controller

--- lib/app/data/module/controller/login_controller.dart
+++ lib/app/data/module/controller/login_controller.dart
@@ -1,4 +1,5 @@
 import 'package:absensi/app/data/module/view/bloc/login_blocs.dart';
+import 'package:absensi/common/widgets/flutter_toast.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,10 +15,13 @@ class LoginController{
         String emailAddress = state.email;
         String password = state.password;
         if(emailAddress.isEmpty){
-          print("email kosong");
+          toastInfo(msg: "Email harus di isi");
+          return;
         }
         if(password.isEmpty){
-          print("password kosong");
+         toastInfo(msg: "password harus di isi");
+         print(toastInfo(msg: "msg"));
+         return;
         }
         try{
           final credential = await FirebaseAuth
@@ -26,16 +30,19 @@ class LoginController{
               email: emailAddress,
               password: password);
           if(credential.user==null){
-            //
+            toastInfo(msg: "kosong");
+            return;
           }
           if(!credential.user!.emailVerified){
-            //
+            toastInfo(msg: "email belum diverifikasi");
+            return;
           }
           var user = credential.user;
           if(user!=null){
-            //
+            print("user ada");
           } else{
-            //
+            toastInfo(msg: "user tidak terdaftar");
+            return;
           }
         }catch(e){
           //
